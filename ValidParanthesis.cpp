82f2b0066e9952bff2900ebdc329d47507e92563
Kartik Gwaliyan <kartikgwaliyan123@gmail.com>
2025-09-18 14:45:03 +0530

    new file

---
+++ ValidParanthesis.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    char expr[100];
+    char stack[100];
+    int top = -1;
+
+    cout << "Enter parentheses string: ";
+    cin >> expr;
+
+    for (int i = 0; expr[i] != '\0'; i++) {
+        char ch = expr[i];
+
+        // Push opening brackets
+        if (ch == '(' || ch == '{' || ch == '[') {
+            top++;
+            stack[top] = ch;
+        }
+        // Check and pop matching closing brackets
+        else if (ch == ')' || ch == '}' || ch == ']') {
+            if (top == -1) {
+                cout << "Invalid parentheses\n";
+                return 0;
+            }
+            char open = stack[top];
+            if ((ch == ')' && open != '(') ||
+                (ch == '}' && open != '{') ||
+                (ch == ']' && open != '[')) {
+                cout << "Invalid parentheses\n";
+                return 0;
+            }
+            top--;
+        }
+    }
+
+    if (top == -1)
+        cout << "Valid parentheses\n";
+    else
+        cout << "Invalid parentheses\n";
+
+    return 0;
+}
